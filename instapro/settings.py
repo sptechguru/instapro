371b2d04b0e51d2a8033acfa56c2b92f2bc97929
santosh <santoshpal61437@gmail.com>
2020-07-12 10:39:50 +0530

    second

--- instapro/settings.py
+++ instapro/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
 }
 
 
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -131,14 +134,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [  os.path.join(BASE_DIR, 'static')   ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = "/"
 
-PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
-MEDIA_ROOT = PROJECT_ROOT + '/static/'
-MEDIA_URL = '/media/' 
+# PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+
 
 
 MESSAGE_TAGS = {
